3219655c93413a8ebfcb87424f868256aa2972e3
Yo <you@enene.com>
2026-04-22 08:27:27 +0300

    fix sql again

--- app/api/room.py
+++ app/api/room.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException
 
+from typing import Optional
 from app.api.deps import get_current_user_profile, require_session_payload
 from app.services.matchmaking_service import find_room_for_user
 from app.services.room_service import get_room_by_token, get_room_members
@@ -84,3 +85,84 @@ def get_room(
         "game": room["game"],
         "join_cost": room["join_cost"]
     }
+
+
+
+
+
+
+"""
+    SHOP
+"""
+
+
+
+shop_router = APIRouter(prefix="/shop", tags=["Shop"])
+router.include_router(shop_router, tags=["Shop"])
+
+
+
+
+@shop_router.post("/{room_access_token}/buy/slot")
+def shop_buy_slot(
+    room_access_token: str,
+    _payload: dict = Depends(require_session_payload),
+):
+    """
+    Купить слот для текущей комнаты.
+    Проверяем в какой стадии находится комната,
+        если это стадия shop, и оцениваем там же нас на победу в функции pgsql
+
+    """
+
+    room = get_room_by_token(room_access_token)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    players = get_room_members(room["id"])
+
+    #if players[''] < len(room['players']):
+
+    return {
+        "id": room["id"],
+        "status": room["status"],
+        "players": players,
+        "game": room["game"],
+        "join_cost": room["join_cost"]
+    }
+
+
+
+@shop_router.post("/{room_access_token}/buy/bust")
+def shop_buy_bust_on_slot(
+    room_access_token: str,
+    slot_id: Optional[int] = None,
+    _payload: dict = Depends(require_session_payload),
+):
+    """
+    Купить буст для выбранного слота.
+    Проверяем в какой стадии находится комната,
+        если это стадия shop, и оцениваем там же нас на победу в функции pgsql
+
+    """
+
+    room = get_room_by_token(room_access_token)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    players = get_room_members(room["id"])
+
+    print(players)
+
+
+
+@shop_router.get("/{room_access_token}/victory_chance")
+def shop_victory_chance(
+    room_access_token: str,
+    _payload: dict = Depends(require_session_payload),
+):
+    """
+    Возвращает все слоты и их шансы с параметром буста.
+    """
+    pass
+
